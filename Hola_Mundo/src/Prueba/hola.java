75b1aefaebe4c89f4daf5cff7b91bff63bae1597
dpalomero <dpalomero@dpalomero>
2020-09-24 15:44:17 +0200

    serializado

--- Hola_Mundo/src/Prueba/hola.java
+++ Hola_Mundo/src/Prueba/hola.java
@@ -1,6 +1,13 @@
 package Prueba;
 
-public class hola {
+import java.io.Serializable;
+
+public class hola implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
