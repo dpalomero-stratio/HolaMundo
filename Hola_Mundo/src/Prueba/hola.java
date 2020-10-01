b172e0ad5fc985b4860ca4fa8798fcd438fb37cd
dpalomero-stratio <71762866+dpalomero-stratio@users.noreply.github.com>
2020-10-01 16:49:29 +0200

    Update hola.java

--- Hola_Mundo/src/Prueba/hola.java
+++ Hola_Mundo/src/Prueba/hola.java
@@ -1,13 +1,4 @@
-package Prueba;
-
-import java.io.Serializable;
-
-public class hola implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
+public class hola {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
