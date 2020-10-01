6d31a7df3f5a421e01ed1f21d18c9b1e13178882
dpalomero-stratio <71762866+dpalomero-stratio@users.noreply.github.com>
2020-10-01 17:00:01 +0200

    Update hola.java

--- Hola_Mundo/src/Prueba/hola.java
+++ Hola_Mundo/src/Prueba/hola.java
@@ -1,6 +1,6 @@
 public class hola {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo!!!");
 	}
 }
